292c0f1dcd23040194048f9fe7b81bd4a0ad92fe
Rammah <ramahnore@gmail.com>
2023-06-18 16:53:23 +0400

    added reusable modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import "./PlaceItem.css";
 import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 
 const PlaceItem = ({ place }) => {
+	const [showMap, setShowMap] = useState(false);
+
+	const openMap = () => setShowMap(true);
+	const closeMap = () => setShowMap(false);
+
 	return (
-		<li className="place-item">
-			<Card>
-				<div className="place-item__image">
-					<img src={place.image} alt={place.title} />
+		<>
+			<Modal
+				show={showMap}
+				onCancel={closeMap}
+				header={place.address}
+				contentClass="place-item__modal-content"
+				footerClass="place-item__modal-actions"
+				footer={<Button onClick={closeMap}>Close Map</Button>}>
+				<div className="map-container">
+					<h2>Map</h2>
 				</div>
-				<div className="place-item__info">
-					<h2>{place.title}</h2>
-					<h3>{place.address}</h3>
-					<p>{place.description}</p>
-				</div>
-				<div className="place-item__actions">
-					<Button inverse>View On Map</Button>
-					<Button to={`/places/${place.id}`}>Edit</Button>
-					<Button danger>Delete</Button>
-				</div>
-			</Card>
-		</li>
+			</Modal>
+			<li className="place-item">
+				<Card>
+					<div className="place-item__image">
+						<img src={place.image} alt={place.title} />
+					</div>
+					<div className="place-item__info">
+						<h2>{place.title}</h2>
+						<h3>{place.address}</h3>
+						<p>{place.description}</p>
+					</div>
+					<div className="place-item__actions">
+						<Button inverse onClick={openMap}>
+							View On Map
+						</Button>
+						<Button to={`/places/${place.id}`}>Edit</Button>
+						<Button danger>Delete</Button>
+					</div>
+				</Card>
+			</li>
+		</>
 	);
 };
 
