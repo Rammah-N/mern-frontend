f5e09ae0fbafc8875756dae6a236c8c4958629d3
Rammah <ramahnore@gmail.com>
2023-06-26 19:41:16 +0400

    feat: hid nav links from unauthenticated users

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { AuthContext } from "../../context/authContext";
 import { NavLink } from "react-router-dom";
 import "./NavLinks.css";
+import Button from "../FormElements/Button";
+
 const NavLinks = () => {
+	const auth = useContext(AuthContext);
 	return (
 		<ul className="nav-links">
 			<li>
@@ -9,15 +14,24 @@ const NavLinks = () => {
 					All Users
 				</NavLink>
 			</li>
-			<li>
-				<NavLink to="/u1/places">My Places</NavLink>
-			</li>
-			<li>
-				<NavLink to="/places/new">Add Place</NavLink>
-			</li>
-			<li>
-				<NavLink to="/auth">Authenticate</NavLink>
-			</li>
+			{auth.isLoggedIn && (
+				<>
+					<li>
+						<NavLink to="/u1/places">My Places</NavLink>
+					</li>
+					<li>
+						<NavLink to="/places/new">Add Place</NavLink>
+					</li>
+					<li>
+						<Button inverse onClick={auth.logout}>Logout</Button>
+					</li>
+				</>
+			)}
+			{!auth.isLoggedIn && (
+				<li>
+					<NavLink to="/auth">Authenticate</NavLink>
+				</li>
+			)}
 		</ul>
 	);
 };
