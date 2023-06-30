1f599326f8939c1b9d5d585bdd515661a3b2bdcd
Rammah <ramahnore@gmail.com>
2023-06-30 14:29:50 +0400

    feat(places): integreated API with current user's places

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,42 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlacesList from "../components/PlacesList";
 import { useParams } from "react-router-dom";
+import { useHttp } from "../../shared/hooks/httpHook";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+
+const API = process.env.REACT_APP_API;
 
 const UserPlaces = () => {
 	const { userId } = useParams();
-	const places = [
-		{
-			id: 1,
-			image: "https://dummyimage.com/400x400/000/fff",
-			title: "Park",
-			description: "A beautiful park with lush greenery and walking trails.",
-			address: "123 Main St, City, Country",
-			creator: "1",
-			coordinates: { lat: 40.7128, lng: -74.006 },
-		},
-		{
-			id: 2,
-			image: "https://dummyimage.com/400x400/000/fff",
-			title: "Restaurant",
-			description:
-				"A cozy restaurant with delicious cuisine and friendly staff.",
-			address: "456 Elm St, City, Country",
-			creator: "2",
-			coordinates: { lat: 34.0522, lng: -118.2437 },
-		},
-		{
-			id: 3,
-			image: "https://dummyimage.com/400x400/000/fff",
-			title: "Museum",
-			description: "An intriguing museum showcasing art and history exhibits.",
-			address: "789 Oak St, City, Country",
-			creator: "3",
-			coordinates: { lat: 51.5074, lng: -0.1278 },
-		},
-	];
+	const [places, setPlaces] = useState(null);
+	const { loading, error, sendRequest, clearError } = useHttp();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data = await sendRequest(`${API}/places/user/${userId}`);
+				setPlaces(data);
+			} catch (error) {}
+		};
+		fetchData();
+	}, [sendRequest, userId]);
+
+	console.log(userId);
 
 	return (
-		<PlacesList places={places.filter((place) => place.creator === userId)} />
+		<>
+			<ErrorModal error={error} onClear={clearError} />
+			{loading && <LoadingSpinner asOverlay />}
+			<PlacesList
+				places={places?.filter((place) => place.creator === userId)}
+			/>
+		</>
 	);
 };
 
